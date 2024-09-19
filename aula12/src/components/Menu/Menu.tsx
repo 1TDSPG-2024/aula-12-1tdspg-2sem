4a4afabb03344e5f5a93e9f99a3f9e6ce9c34726
Caepena <caepenafiel@gmail.com>
2024-09-19 11:43:25 -0300

    Realizando deploy da aplicação na nuvem da Vercel e criando estruturas de roas com componentes funcionas

---
+++ aula12/src/components/Menu/Menu.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Cabecalho() {
+    return(
+        <nav>
+            <ul>
+                <li><Link href="/">Home</Link></li>
+            </ul>
+        </nav>
+    )
+}
